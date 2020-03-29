ecdee6976887c697e724580a1e265ffbdab3ae51
changzhengwan <changzhengwan@ecarx.com.cn>
2020-03-29 16:59:12 +0800

    feat: 添加db骨骼动画demo
    Signed-off-by: changzhengwan <changzhengwan@ecarx.com.cn>

--- app/src/main/java/com/wanwan/animation/MainActivity.java
+++ app/src/main/java/com/wanwan/animation/MainActivity.java
@@ -2,13 +2,111 @@ package com.wanwan.animation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import org.egret.egretnativeandroid.EgretNativeAndroid;
+import org.egret.runtime.launcherInterface.INativePlayer;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final String TAG = "MainActivity";
+    private EgretNativeAndroid nativeAndroid;
+    private FrameLayout flAnimation;
+    private Button btnThink, btnHello;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        flAnimation = findViewById(R.id.fl_animation);
+        btnThink = findViewById(R.id.btn_think);
+        btnHello = findViewById(R.id.btn_hello);
+
+        btnThink.setOnClickListener(this);
+        btnHello.setOnClickListener(this);
+
+        nativeAndroid = new EgretNativeAndroid(this);
+        if (!nativeAndroid.checkGlEsVersion()) {
+            Toast.makeText(this, "This device does not support OpenGL ES 2.0.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        nativeAndroid.config.showFPS = false;
+        nativeAndroid.config.fpsLogTime = 15;
+        nativeAndroid.config.disableNativeRender = false;
+        nativeAndroid.config.clearCache = false;
+        nativeAndroid.config.loadingTimeout = 0;
+
+        setExternalInterfaces();
+
+        if (!nativeAndroid.initialize("http://tool.egret-labs.org/Weiduan/game/index.html")) {
+            Toast.makeText(this, "Initialize native failed.",
+                    Toast.LENGTH_LONG).show();
+            return;
+        }
+        flAnimation.removeAllViews();
+        flAnimation.addView(nativeAndroid.getRootFrameLayout());
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        nativeAndroid.pause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nativeAndroid.resume();
+    }
+
+    @Override
+    public boolean onKeyDown(final int keyCode, final KeyEvent keyEvent) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            nativeAndroid.exitGame();
+        }
+
+        return super.onKeyDown(keyCode, keyEvent);
+    }
+
+    private void setExternalInterfaces() {
+        nativeAndroid.setExternalInterface("sendToNative", new INativePlayer.INativeInterface() {
+            @Override
+            public void callback(String message) {
+                String str = "Native get message: ";
+                str += message;
+                Log.d(TAG, str);
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == R.id.btn_think){
+            nativeAndroid.callExternalInterface("sendToJS", "think");
+        }else if(v.getId() == R.id.btn_hello){
+            nativeAndroid.callExternalInterface("sendToJS", "hello");
+//            nativeAndroid.callExternalInterface("sendToJS", bitmap);
+        }
+    }
+
+    private void sendTexPng(){
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
+        nativeAndroid.callExternalInterface("sendToJS", bitmap.toString());
     }
 }
